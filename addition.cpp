0bc5330c39a1c271a346ad8abfebea7721c882d8
rorManish09 <manishchaudhary4970@gmail.com>
2023-01-08 15:49:16 +0530

    Addition program

---
+++ addition.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Adiition ";
+    int a,b;
+    cin>>a>>b;
+    cout<<"a+b= "<<a+b;
+}
---
+++ world.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=12,b=2;
+    cout<<"A+B="<<a+b;
+}
